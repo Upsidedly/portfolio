6266fcaaaf0f6522a92764cd3787c831b3462fed
Upsidedly <matthewlloydw@gmail.com>
2023-10-11 22:19:17 -0500

    feat: extreme badness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,54 @@
-import Image from 'next/image'
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+import { Icon } from '@iconify/react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="w-full h-full fixed">
+      <Image
+        src="/all-of-me.jpeg"
+        quality={100}
+        placeholder="blur"
+        blurDataURL="/all-of-me.jpeg"
+        fill
+        sizes="100vw"
+        alt="All of Me"
+        style={{ objectFit: "cover" }}
+      />
+      <div
+        className="w-full h-full absolute flex items-center justify-center"
+        style={{
+          backdropFilter: "blur(10px)",
+          backgroundColor: "rgba(20, 20, 20, 0.8)",
+        }}
+      >
+        <div className="w-[50rem]">
+          <article>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center w-full text-white/85">
+              Upsided
+            </h1>
+            <p className="leading-7 [&:not(:first-child)]:my-[2rem] text-xl px-[6.3rem] text-center">
+A dude who enjoys programming, Terraria, making music, and embarking on incredibly arbitrary coding projects that will never see the <span className="text-transparent bg-clip-text bg-gradient-to-t from-yellow-200 to-yellow-50">light of day</span>. I&apos;m also active on discord I guess.
+            </p>
+            <div className="w-full h-[10rem] flex flex-row gap-5 justify-center">
+              {...([
+              [<Icon key={1} icon="ic:baseline-discord" className="w-[2.5rem] h-[2.5rem]" />, 'https://discord.gg/PKCTcBPB8r'],
+              [<Icon key={2} icon="mdi:youtube" className="w-[2.5rem] h-[2.5rem]" />, 'https://www.youtube.com/channel/UCDXeDbeq-Z6ngCASDZ5Dmzg'],
+              [<Icon key={3} icon="mdi:github" className="w-[2.5rem] h-[2.5rem]" />, 'https://github.com/Upsidedly']
+            ] as [JSX.Element, string][])
+                .map((v) => <Link href={v[1]} key={v[0].key}><div
+                  className="h-[5rem] w-[5rem] bg-opacity-30 hover:bg-opacity-80 bg-slate-950 duration-200 flex justify-center items-center rounded-full aspect-square">
+                    {v[0]}
+                </div></Link>)}
+            </div>
+          </article>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className="fixed bottom-0 flex justify-center items-center w-full h-10 border border-black/10 bg-slate-950 bg-opacity-10">
+        <p className="text-slate-400">Background is the cover of Masayoshi Takanaka&apos;s compilation album <Link href="https://www.youtube.com/watch?v=BNig6xG36i4&t=53s&pp=ygUJYWxsIG9mIG1l" target="_blank" className="underline-offset-4 hover:underline font-semibold">All of Me</Link> </p>
+      </footer>
     </main>
-  )
+  );
 }
